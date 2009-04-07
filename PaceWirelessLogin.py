ad9baef4cc18b8c81fdf8310c855d7683c982329
Brandon Schlueter <brandon@brandonschlueter.com>
2009-04-07 16:44:07 -0400

    Short and long command line options for password, username, logout and verbosity work. Logout functionality is in progress.

--- PaceWirelessLogin.py
+++ PaceWirelessLogin.py
@@ -39,12 +39,10 @@ def pwn_login(name,password, verbose):
 	            print "Login Failed! Unknown error."
     crl.close()
 
-def pwn_logout():
-    login_url='https://pacewireless.pace.edu/login.pl'
+def pwn_logout(verbose):
+    login_url='https://pacewireless.pace.edu/login.pl'+open('.logout.info','r').readline()
     buffer = StringIO()
     crl = pycurl.Curl()
-    logout_form_data = 'action=logout;r='+open('.logout.info','r').readline()
-    crl.setopt(pycurl.POSTFIELDS, logout_form_data)
     crl.setopt(pycurl.URL, login_url)
     crl.setopt(pycurl.WRITEFUNCTION, buffer.write)
     crl.perform()
@@ -54,41 +52,40 @@ def pwn_logout():
         logout_regex = re.compile('You are already logged in')
         if logout_regex.search(buffer.getvalue()):
             print "Successful logout."
-        except IndexError:
+    except IndexError:
             try:
                 invalid_login_regex = re.compile('This link is not valid for your current session.')
-                if invalid_login_regex.search(buffer.getvalue()):
+                alt_invalid_regex = re.compile('404 - File Not Found')
+                if invalid_login_regex.search(buffer.getvalue()) or alt_invalid_regex.search(buffer.getvalue()):
                     print "Invalid data, the logout information was most likely corrupt."
-                except IndexError:
+            except IndexError:
                     print "Login Failed! Unknown error."
     crl.close()
-    os.remove('.logout.info')
+    #os.remove('.logout.info')
 
 def main():
     # Parse command line options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'vl:up', ['logout','username=','password='])
+        opts, args = getopt.getopt(sys.argv[1:], 'vlu:p:', ['logout','username=','password='])
     except getopt.error, msg:
         print ('python PaceWirelessLogin [-v] [-l| -u username -p password] [--logout| --username=username --password=password]')
         sys.exit(2)
-
     name,password = '',''
-     # Process options
-     
+    # Process options
+    verbose = '-v' in dict(opts)
     if '--logout' in dict(opts) or '-l' in dict(opts):
-        pwn_logout()
-    else:    
+        pwn_logout(verbose)
+    else:
         for option, arg in opts:
             if option == '--username' or option == '-u':
                 name = arg
             elif option == '--password' or option == '-p':
                 password = arg
-
         while not name:
             name = raw_input('Please enter Pace username: ')
         while not password:
             password = raw_input('Please enter Pace password: ')
-        pwn_login(name,password, '-v' in opts)
+        pwn_login(name, password, verbose)
 
 if __name__=='__main__':
     main()
